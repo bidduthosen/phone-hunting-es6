31385da717416c589e2b1cf083bd3787d788d4a1
bidduthosen <mdbidduthosen34@gmail.com>
2025-02-15 19:53:22 +0600

    dynamic loaded phone details data load

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
+// fetch all phone api data load and loader data-------------
 const loadAllPhone = async (status, searchText) => {
     document.getElementById('loader').classList.add('hidden');
-
 
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText ? searchText : 'iphone'}`);
     const data = await res.json()
@@ -13,6 +13,15 @@ const loadAllPhone = async (status, searchText) => {
     }
 }
 
+// fetch phone details api data-------------------------------
+const loadPhoneDetails = async(id) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    console.log(data.data.slug)
+}
+
+
+// search this input field click handler
 const handleSearch = () => {
     document.getElementById('loader').classList.remove('hidden');
     const inputNameField = document.getElementById('input-brand-name').value;
@@ -23,11 +32,13 @@ const handleSearch = () => {
 
 }
 
-
+// display show all phone data
 const displayAllPhoen = (items) => {
     const allPhoneContainer = document.getElementById('allPhone-container');
     items.map(item => {
+        // distructuring-- object------------
         const { slug, phone_name, image, brand } = item
+
         const div = document.createElement('div');
         div.innerHTML =
             `
@@ -41,8 +52,9 @@ const displayAllPhoen = (items) => {
                 <div class="card-body items-center text-center">
                     <h2 class="card-title">${brand}</h2>
                     <p>${phone_name}</p>
+                    <p>${slug}</p>
                     <div class="card-actions">
-                    <button class="btn btn-primary">Buy Now</button>
+                    <button onclick="loadPhoneDetails('${slug}')" class="btn btn-primary">Buy Now</button>
                     </div>
                 </div>
             </div>
